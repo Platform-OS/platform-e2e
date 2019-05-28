c54b89867a75aeb606944d18474d64aa23d32780
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2019-05-28 17:13:22 +0200

    Data import export: use mpkit with data import that waits until finished.

--- modules/import_export/test/index.js
+++ modules/import_export/test/index.js
@@ -3,7 +3,7 @@ import faker from 'faker';
 import Home from './home-page';
 import sh from 'shelljs';
 const fs = require('fs');
-sh.config.silent = true;
+// sh.config.silent = true;
 
 const home = new Home();
 const sleep = (ms) => {
@@ -21,7 +21,6 @@ test.before(async t => {
   let command = sh.exec(`marketplace-kit data import -p ./modules/import_export/test/fixtures/empty_json.json`);
   await t.expect(command.code).eql(0);
 })('Import empty file', async t => {
-  await sleep(5000); // TODO: import could wait for completion
   await t.navigateTo('/import_export');
   const results = JSON.parse(await home.results.innerText);
   await t.expect(results.models.total_entries).eql(0);
@@ -31,7 +30,6 @@ test.before(async t => {
   let command = sh.exec(`marketplace-kit data import -p ./modules/import_export/test/fixtures/data.json`);
   await t.expect(command.code).eql(0);
 })('Import data file', async t => {
-  await sleep(5000);
   await t.navigateTo('/import_export');
   const results = JSON.parse(await home.results.innerText);
 
@@ -43,7 +41,6 @@ test.before(async t => {
   let command = sh.exec(`marketplace-kit data import -p ./modules/import_export/test/fixtures/data.json`);
 
   await t.expect(command.code).eql(0);
-  await sleep(5000);
 })('Export imported data', async t => {
   const filename = 'modules/import_export/test/fixtures/data-exported.json';
   let command = sh.exec(`marketplace-kit data export -p ${filename}`);
